437cc7e83c7b825a223ee5761909b9f8649c2460
Burak Celik <burak.celik@mentormate.com>
2023-08-22 12:11:25 +0300

    mongo db conn edit

--- server/dist/utils/connectDB.js
+++ server/dist/utils/connectDB.js
@@ -11,16 +11,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.set('strictQuery', false);
-var config = require('config');
-const dbUrl = `mongodb+srv://webflowdb:wvWoEvkHUHgcSKGP@web-flow-dsml-cluster.cmisbke.mongodb.net/?retryWrites=true&w=majority`;
+mongoose_1.default.set("strictQuery", false);
+var config = require("config");
+const dbUrl = (_a = process.env.MONGO_DB_URL) !== null && _a !== void 0 ? _a : "null"; //`mongodb+srv://webflowdb:wvWoEvkHUHgcSKGP@web-flow-dsml-cluster.cmisbke.mongodb.net/?retryWrites=true&w=majority`;
 // mongodb+srv://webflowdb:wvWoEvkHUHgcSKGP@web-flow-dsml-cluster.cmisbke.mongodb.net/?retryWrites=true&w=majority
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield mongoose_1.default.connect(dbUrl);
-        console.log('Database connected...');
+        console.log("Database connected...");
     }
     catch (error) {
         console.log(error.message);
